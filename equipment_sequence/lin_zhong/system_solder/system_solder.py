9cba863dbd1084be4796b55c2701cfbf6692e2ba
LiuWei <183074632@qq.com>
2025-07-05 22:36:58 +0800

    feature: update system solder

--- equipment_sequence/lin_zhong/system_solder/system_solder.py
+++ equipment_sequence/lin_zhong/system_solder/system_solder.py
@@ -1,57 +1,70 @@
 """
-    半桥请求进站
-        PC -> MES: {"bridge_request": "bridge_code"}
-        MES -> PC: 1: 可做, 2: 不可做
-    焊接盘进站
-        PC -> MES: {"carrier_in": "carrier_code"}
-        MES -> PC: OK
-    带有产品和半桥的焊接托盘出站
-        PC -> MES:
-            {
-                "carrier_out": {
-                    "carrier_code": "carrier_code_value",
-                    "product_codes": ["code1", "code2"],
-                    "bridge_codes: ["bridge_code1", "bridge_code2", "bridge_code3", "bridge_code4", "bridge_code5", "bridge_code6"]
-                }
+半桥请求进站
+    PC -> MES: {"bridge_request": "bridge_code"}
+    MES -> PC: 1: 可做, 2: 不可做
+
+焊接盘进站
+    PC -> MES: {"carrier_in": "carrier_code"}
+    MES -> PC:
+        {
+            "lot_name": "lot_name_value",
+            "circulate_name": "circulate_name_value",
+            "product_type": "product_type_value",
+            "recipe_name": "SA6",
+            "product_code_list": ["product_code1", "product_code2"]
+        }
+
+带有产品和半桥的焊接托盘出站
+    PC -> MES:
+        {
+            "carrier_out": {
+                "lot_name": "lot_name_value",
+                "circulate_name": "circulate_name_value",
+                "product_type": "product_type_value",
+                "recipe_name": "SA6",
+                "carrier_code": "carrier_code_value",
+                "product_codes": ["code1", "code2"],
+                "bridge_codes": ["bridge_code1", "bridge_code2", "bridge_code3", "bridge_code4", "bridge_code5", "bridge_code6"]
             }
-        MES -> PC: OK
+        }
+    MES -> PC: OK
 
+控制状态改变
+    PC -> MES: {"control_state_change": "1"}
+    MES -> PC: OK
 
-    控制状态改变
-        PC -> MES: {"control_state_change": "当前控制状态的值"}
-        MES -> PC: OK
-    运行状态改变
-        PC -> MES: {"machine_state_change": "当前运行状态的值"}
-        MES -> PC: OK
-    切换配方
-        MES -> PC: {"pp_select": "recipe_name_value"}
-        PC -> MES: {"pp_select_result": 1}, 1: 切换成功, 2: 切换失败
-    询问当前配方
-        MES -> PC: current_recipe_name
-        PC -> MES: {"current_recipe_name": "recipe_name_value"}
-    出现报警
-        PC -> MES: {"occur_alarm": {"alarm_id": 100,"alarm_text":"this is alarm content"}}
-        MES -> PC: OK
-    报警消除
-        PC -> MES: {"clean_alarm": {"alarm_id": 100,"alarm_text":"this is alarm content"}}
-        MES -> PC: OK
+运行状态改变
+    PC -> MES: {"machine_state_change": "1"}
+    MES -> PC: OK
+
+出现报警
+    PC -> MES: {"occur_alarm": {"alarm_id": 100,"alarm_text":"this is alarm content"}}
+    MES -> PC: OK
+报警消除
+
+    PC -> MES: {"clean_alarm": {"alarm_id": 100,"alarm_text":"this is alarm content"}}
+    MES -> PC: OK
 """
+import json
+import time
+
 from inovance_tag.tag_communication import TagCommunication
 from passive_equipment.handler_passive import HandlerPassive
 from socket_cyg.socket_server_asyncio import CygSocketServerAsyncio
 
 from equipment_sequence.lin_zhong.system_solder import table_models
+from equipment_sequence.lin_zhong.system_solder.table_models import Uploading
 
 
 class SystemSolder(HandlerPassive):
 
     def __init__(self):
         control_dict = {
-            "uploading_tag": TagCommunication("127.0.0.1"),
+            "uploading_tag": TagCommunication("10.21.142.10"),
             "place_solder_sheet": CygSocketServerAsyncio("127.0.0.1", 1830),
-            "cutting_tag": TagCommunication("127.0.0.2")
+            "cutting_tag": TagCommunication("10.21.142.60")
         }
-        super().__init__(__file__, control_dict, open_flag=False)
+        super().__init__(__file__, control_dict, open_flag=True)
 
     def _on_rcmd_LotStartReply(self, **kwargs):
         """eap监控到 1006 new_circulate 事件后下发的 LotStartReply.
@@ -59,6 +72,24 @@ class SystemSolder(HandlerPassive):
         Args:
             **kwargs:
         """
+        current_recipe_name = kwargs.get("PPID", "")
+        current_lot_name = kwargs.get("Circulate_StockNo", "")
+        circulate_name = kwargs.get("Circulate_No", "")
+        product_type = kwargs.get("Circulate_item", "")
+        lot_quantity = int(kwargs.get("Circulate_Qty", 0))
+        self.set_sv_value_with_name("current_recipe_name", current_recipe_name)
+        self.set_sv_value_with_name("current_lot_name", current_lot_name)
+        self.set_sv_value_with_name("lot_quantity", lot_quantity)
+
+        self.config_instance.update_config_sv_value("current_recipe_name", current_recipe_name)
+        self.config_instance.update_config_sv_value("current_lot_name", current_lot_name)
+        self.config_instance.update_config_sv_value("lot_quantity", lot_quantity)
+
+        self.set_dv_value_with_name("circulate_name", circulate_name)
+        self.set_dv_value_with_name("product_type", product_type)
+
+        self.config_instance.update_config_dv_value("circulate_name", circulate_name)
+        self.config_instance.update_config_dv_value("product_type", product_type)
 
     def _on_rcmd_keyCarrierReply(self, **kwargs):
         """eap监控到 2003 solder_carrier_in_request_uploading 回复回流焊托盘是否可以进站.
@@ -97,6 +128,32 @@ class SystemSolder(HandlerPassive):
         self.set_dv_value_with_name("circulate_name", circulate_name)
         self.set_dv_value_with_name("is_eap_reply_bridge_in", True)
 
+    def control_state_change(self, current_control_state: str) -> str:
+        """放半桥设备控制状态变化.
+
+        Args:
+            current_control_state: 放半桥设备当前控制状态.
+
+        Returns:
+            str: 返回 OK.
+        """
+        self.set_sv_value_with_name("current_control_state_bridge", int(current_control_state))
+        self.send_s6f11("control_state_change_bridge")
+        return "OK"
+
+    def machine_state_change(self, current_machine_state: str) -> str:
+        """放半桥设备运行状态变化.
+
+        Args:
+            current_machine_state: 放半桥设备当前运行状态.
+
+        Returns:
+            str: 返回 OK.
+        """
+        self.set_sv_value_with_name("current_machine_state_bridge", int(current_machine_state))
+        self.send_s6f11("machine_state_change_bridge")
+        return "OK"
+
     def new_circulate(self, circulate_name: str) -> str:
         """在设备上开工单.
 
@@ -134,7 +191,7 @@ class SystemSolder(HandlerPassive):
         Returns:
             str: 返回半桥是否可做, 1: 可做, 2: 不可做.
         """
-        self.set_dv_value_with_name("bridge_code", bridge_code)
+        self.set_dv_value_with_name("bridge_code_in_request", bridge_code)
         self.send_s6f11("bridge_in_request")
 
         self.wait_eap_reply(self.config["socket_signal_info"]["bridge_request"])
@@ -183,7 +240,25 @@ class SystemSolder(HandlerPassive):
         """
         self.set_dv_value_with_name("solder_carrier_code_in_bridge", carrier_code)
         self.set_time_dv_value({"dv_name": "solder_carrier_in_time_bridge"})
-        return "OK"
+
+        product_info_list = self.mysql.query_data(Uploading, {"solder_carrier_code": carrier_code})
+        product_code_list = []
+        one_product_info = product_info_list[0]
+        lot_name = one_product_info.get("lot_name", "")
+        circulate_name = one_product_info.get("circulate_name", "")
+        product_type = one_product_info.get("product_type", "")
+        recipe_name = one_product_info.get("recipe_name", "")
+        for product_info in product_info_list:
+            product_code_list.append(product_info["product_code"])
+
+        response_data = {
+            "lot_name": lot_name,
+            "circulate_name": circulate_name,
+            "product_type": product_type,
+            "recipe_name": recipe_name,
+            "product_code_list": product_code_list
+        }
+        return json.dumps(response_data)
 
     def carrier_out(self, carrier_info: dict) -> str:
         """焊接托盘出放半桥设备.
@@ -199,6 +274,18 @@ class SystemSolder(HandlerPassive):
         Returns:
             str: 返回 OK.
         """
+        lot_name = carrier_info.get("lot_name", "")
+        self.set_sv_value_with_name("current_lot_name", lot_name)
+
+        recipe_name = carrier_info.get("recipe_name", "")
+        self.set_sv_value_with_name("current_recipe_name", recipe_name)
+
+        circulate_name = carrier_info.get("circulate_name", "")
+        self.set_dv_value_with_name("circulate_name", circulate_name)
+
+        product_type = carrier_info.get("product_type", "")
+        self.set_dv_value_with_name("product_type", product_type)
+
         solder_carrier_code_out_bridge = carrier_info.get("carrier_code", "")
         self.set_dv_value_with_name("solder_carrier_code_out_bridge", solder_carrier_code_out_bridge)
 
@@ -213,7 +300,7 @@ class SystemSolder(HandlerPassive):
         self.save_data_to_database_solder_carrier_out_bridge()
         return "OK"
 
-    def set_dv_from_database_solder_carrier_in_cutting(self, call_back: dict):
+    def set_dv_from_database_solder_carrier_in_cutting(self, call_back: dict, **kwargs):
         """下料机焊接托盘进站时从数据库获取数据设置 dv.
 
         Args:
@@ -240,7 +327,7 @@ class SystemSolder(HandlerPassive):
         self.set_dv_value_with_name("bridge_code_list_cutting_write", bridge_list)
         self.set_dv_value_with_name("product_state_list_cutting_write", product_state_list)
 
-    def set_dv_from_database_key_carrier_out_cutting(self, call_back: dict):
+    def set_dv_from_database_key_carrier_out_cutting(self, call_back: dict, **kwargs):
         """下料机键合托盘出站时从数据库获取数据设置 dv.
 
         Args:
@@ -248,27 +335,22 @@ class SystemSolder(HandlerPassive):
         """
         table_model_class_name = call_back["table_model_class_name"]
         table_model_class = getattr(table_models, table_model_class_name)
-        filter_dict = {
-            "key_carrier_code": self.get_dv_value_with_name("key_carrier_code_out_cutting")
-        }
-        data_list = self.mysql.query_data(table_model_class, filter_dict)
-        product_code_set, bridge_list = set(), []
+        product_code_list_cutting = self.get_dv_value_with_name("product_code_list_cutting")
+        data_list = self.mysql.query_data_in(table_model_class, "product_code", product_code_list_cutting)
+        bridge_list = []
         for data in data_list:
-            product_code_set.add(data["product_code"])
             bridge_list.append(data["bridge_code"])
-        product_code_list = list(product_code_set)
 
-        self.set_dv_value_with_name("product_code_list_cutting", product_code_list)
         self.set_dv_value_with_name("bridge_code_list_cutting", bridge_list)
 
     # 焊接托盘出站
-    def save_data_to_database_solder_carrier_out_uploading(self, call_back: dict):
+    def save_data_to_database_solder_carrier_out_uploading(self, call_back: dict, **kwargs):
         """保存焊接托盘从上料机出站数据.
 
         Args:
             call_back: 要执行的 call_back 信息.
         """
-        table_model_class_name = call_back["table_model_class"]
+        table_model_class_name = call_back["table_model_class_name"]
         table_model_class = getattr(table_models, table_model_class_name)
 
         product_code_list_uploading = self.get_dv_value_with_name("product_code_list_uploading")
@@ -279,11 +361,12 @@ class SystemSolder(HandlerPassive):
             add_data_list.append({
                 "product_code": product_code,
                 "solder_carrier_code": self.get_dv_value_with_name("solder_carrier_in_time_uploading"),
-                "solder_carrier_in_time": self.get_dv_value_with_name("solder_carrier_in_time_uploading"),
-                "solder_carrier_out_time": self.get_dv_value_with_name("solder_carrier_out_time_uploading"),
+                "solder_carrier_in_time_uploading": self.get_dv_value_with_name("solder_carrier_in_time_uploading"),
+                "solder_carrier_out_time_uploading": self.get_dv_value_with_name("solder_carrier_out_time_uploading"),
                 "solder_jig_code": solder_jig_code_list[index],
                 "lot_name": self.get_sv_value_with_name("lot_name"),
                 "circulate_name": self.get_dv_value_with_name("circulate_name"),
+                "recipe_name": self.get_sv_value_with_name("current_recipe_name"),
             })
 
         self.mysql.add_data(table_model_class, add_data_list)
@@ -299,29 +382,31 @@ class SystemSolder(HandlerPassive):
                 "bridge_code": bridge_code,
                 "product_code": product_code_list_bridge[0] if index > 2 else product_code_list_bridge[1],
                 "solder_carrier_code": solder_carrier_code_out_bridge,
-                "solder_carrier_in_time": self.get_dv_value_with_name("solder_carrier_in_time_bridge"),
-                "solder_carrier_out_time": self.get_dv_value_with_name("solder_carrier_out_time_bridge"),
-                "lot_name": self.get_dv_value_with_name("current_lot_name"),
+                "solder_carrier_in_time_bridge": self.get_dv_value_with_name("solder_carrier_in_time_bridge"),
+                "solder_carrier_out_time_bridge": self.get_dv_value_with_name("solder_carrier_out_time_bridge"),
+                "lot_name": self.get_sv_value_with_name("current_lot_name"),
+                "recipe_name": self.get_sv_value_with_name("current_recipe_name"),
                 "circulate_name": self.get_dv_value_with_name("circulate_name"),
+                "product_type": self.get_dv_value_with_name("product_type"),
             })
         self.mysql.add_data(table_models.Bridge, add_data)
 
-    def save_data_to_database_solder_carrier_out_cutting(self, call_back: dict):
+    def save_data_to_database_solder_carrier_out_cutting(self, call_back: dict, **kwargs):
         """焊接托盘从下料设备出站保存进出站时间.
 
         Args:
             call_back: 要执行的 call_back 信息.
         """
-        table_model_class_name = call_back["table_model_class"]
+        table_model_class_name = call_back["table_model_class_name"]
         table_model_class = getattr(table_models, table_model_class_name)
 
         solder_carrier_out_time_cutting = self.get_dv_value_with_name("solder_carrier_out_time_cutting")
         solder_carrier_in_time_cutting = self.get_dv_value_with_name("solder_carrier_in_time_cutting")
 
         add_data_list = [{
-            "solder_carrier_code": self.get_dv_value_with_name("solder_carrier_out_cutting"),
-            "solder_carrier_in_time": solder_carrier_in_time_cutting,
-            "solder_carrier_out_time": solder_carrier_out_time_cutting,
+            "solder_carrier_code": self.get_dv_value_with_name("solder_carrier_code_out_cutting"),
+            "solder_carrier_in_time_cutting": solder_carrier_in_time_cutting,
+            "solder_carrier_out_time_cutting": solder_carrier_out_time_cutting,
             "lot_name": self.get_sv_value_with_name("current_lot_name"),
             "circulate_name": self.get_dv_value_with_name("circulate_name"),
         }]
@@ -329,13 +414,13 @@ class SystemSolder(HandlerPassive):
         self.mysql.add_data(table_model_class, add_data_list)
 
     # ------------------
-    def save_data_to_database_product_in_key_carrier(self, call_back: dict):
+    def save_data_to_database_product_in_key_carrier(self, call_back: dict, **kwargs):
         """保存下料机产品放入键合托盘数据.
 
         Args:
             call_back: 要执行的 call_back 信息.
         """
-        table_model_class_name = call_back["table_model_class"]
+        table_model_class_name = call_back["table_model_class_name"]
         table_model_class = getattr(table_models, table_model_class_name)
 
         key_carrier_code_product_in_key = self.get_dv_value_with_name("key_carrier_code_product_in_key")
@@ -353,16 +438,16 @@ class SystemSolder(HandlerPassive):
 
         self.mysql.add_data(table_model_class, add_data_list)
 
-    def save_data_to_database_records(self, call_back: dict):
+    def save_data_to_database_records(self, call_back: dict, **kwargs):
         """焊接托盘从下料设备出站保存此焊接托盘的生命周期.
 
         Args:
             call_back: 要执行的 call_back 信息.
         """
-        table_model_class_name = call_back["table_model_class"]
+        table_model_class_name = call_back["table_model_class_name"]
         table_model_class = getattr(table_models, table_model_class_name)
 
-        solder_carrier_out_cutting = self.get_dv_value_with_name("solder_carrier_out_cutting")
+        solder_carrier_out_cutting = self.get_dv_value_with_name("solder_carrier_code_out_cutting")
         filter_dict = {
             "solder_carrier_code": solder_carrier_out_cutting
         }
@@ -383,13 +468,69 @@ class SystemSolder(HandlerPassive):
             real_data_list.append(data)
         self.mysql.add_data(table_model_class, real_data_list)
 
-    def delete_database_data_solder_carrier_out_cutting(self, call_back: dict):
+    def delete_database_data_solder_carrier_out_cutting(self, call_back: dict, **kwargs):
         """焊接托盘出站删除此焊接托盘的所有信息.
 
         Args:
             call_back: 要执行的 call_back 信息.
         """
-        carrier_code = self.get_dv_value_with_name("solder_carrier_code_out_cutting")
+        carrier_code = self.get_dv_value_with_name("solder_carrier_code_in_pinfin")
         self.mysql.delete_data(table_models.Uploading, {"solder_carrier_code": carrier_code})
         self.mysql.delete_data(table_models.Bridge, {"solder_carrier_code": carrier_code})
         self.mysql.delete_data(table_models.Cutting, {"solder_carrier_code": carrier_code})
+
+    # -------------------------------------
+    def wait_one_second(self, call_back, **kwargs):
+        """等待一秒."""
+        time.sleep(1)
+
+    def read_multiple_update_dv_tag(self, call_back: dict, equipment_name: str):
+        """读取标签通讯 plc 多个数据更新 dv 值.
+
+        Args:
+            call_back: 要执行的 call_back 信息.
+            equipment_name: 设备名称。
+        """
+        plc = self.control_instance_dict.get(equipment_name)
+        value_list = []
+        count_num = call_back["count_num"]
+        start_address = call_back.get("address")
+        for i in range(1, count_num + 1):
+            address_info = {
+                "address": start_address.replace("$", str(i)),
+                "data_type": call_back.get("data_type"),
+            }
+            plc_value = plc.execute_read(**address_info)
+            value_list.append(plc_value)
+        self.set_dv_value_with_name(call_back.get("dv_name"), value_list)
+        self.logger.info("当前 dv %s 值 %s", call_back.get("dv_name"), value_list)
+
+    def write_multiple_dv_value_tag(self, call_back: dict, equipment_name: str):
+        """向标签通讯 plc 地址写入 dv 值.
+
+        Args:
+            call_back: 要执行的 call_back 信息.
+            equipment_name: 设备名称.
+        """
+        value_list = self.get_dv_value_with_name(call_back.get("dv_name"))
+        for i, value in enumerate(value_list, 1):
+            _call_back = {
+                "address": call_back.get("address").replace("$", str(i)),
+                "data_type": call_back.get("data_type"),
+            }
+            self._write_value(_call_back, value, equipment_name)
+
+    def on_sv_value_request(self, svid, status_variable):
+        """Host 请求查询当前配方时, 从 plc 获取实时配方."""
+        if str(status_variable.svid) == "505":
+            plc = self.control_instance_dict["uploading_tag"]
+            current_recipe_id = plc.execute_read("int", "Application.gvl_OPMODE01_MES.RecipeID")
+            current_recipe_name = self.config_instance.get_recipe_name_with_id(current_recipe_id)
+
+            self.set_sv_value_with_name("current_recipe_id", current_recipe_id)
+            self.set_sv_value_with_name("current_recipe_name", current_recipe_name)
+
+            self.config_instance.update_config_sv_value("current_recipe_id", current_recipe_id)
+            self.config_instance.update_config_sv_value("current_recipe_name", current_recipe_name)
+
+        return status_variable.value_type(status_variable.value)
