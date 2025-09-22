07bb6bca01a554bc7dbed8c63e61a03df4ac12d7
LiuWei <183074632@qq.com>
2025-09-22 13:24:38 +0800

    feature: update passive-equipment dependency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from equipment_sequence.chang_fei.jin_sha_jiang.print_label import PrintLabel
 from equipment_sequence.chang_fei.jin_sha_jiang.screw import Screw
-
+from equipment_sequence.chang_fei.jin_sha_jiang.uoload_add_glue import UploadAddGlue
 
 if __name__ == '__main__':
-    Screw()
+    PrintLabel()
