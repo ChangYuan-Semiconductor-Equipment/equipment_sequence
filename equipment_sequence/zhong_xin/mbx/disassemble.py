fc7c3e8ffd8840564cee7732c0b29d4c20c43926
LiuWei <183074632@qq.com>
2025-10-30 11:30:21 +0800

    feature: remove invalid dir

--- equipment_sequence/zhong_xin/mbx/disassemble.py
+++
@@ -1,9 +0,0 @@
-from inovance_tag.tag_communication import TagCommunication
-from passive_equipment.handler_passive import HandlerPassive
-
-
-class Disassemble(HandlerPassive):
-    def __init__(self):
-        control_dict = {"disassemble_tag": TagCommunication("")}
-
-        super().__init__(control_dict)
--- equipment_sequence/zhong_xin/mbx/place_bridge.py
+++
@@ -1,9 +0,0 @@
-from inovance_tag.tag_communication import TagCommunication
-from passive_equipment.handler_passive import HandlerPassive
-
-
-class PlaceBridge(HandlerPassive):
-    def __init__(self):
-        control_dict = {"place_bridge_tag": TagCommunication("")}
-
-        super().__init__(control_dict)
